3710ab1039a48d16ad14e1c9d1ce1cb065c6dd80
losthj <thislost@gmail.com>
2019-07-19 11:55:38 +0800

    update4

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import { CSSTransition } from 'react-transition-group';
+import {actionCreators} from './store/index';
 import {HeaderWrapper,Logo,Nav,NavItem,NavSearch,Addition,Button,Searchwrapper} from './style';
 
 const Header =(props)=>{
@@ -45,16 +46,10 @@ const mapStateToProps = (state)=>{
 const mapDispathToProps = (dispatch)=>{
     return{
         handleInputFocus(){
-            const action = {
-                type:'search_focus'
-            }
-            dispatch(action)
+            dispatch(actionCreators.searchFocus())
         },
         handleInputBlur(){
-            const action = {
-                type:'search_blur'
-            }
-            dispatch(action)
+            dispatch(actionCreators.searchblur())
         }
     }
 }
--- src/common/header/store/reducer.js
+++ src/common/header/store/reducer.js
@@ -1,14 +1,26 @@
+import * as constants from './constants';
+
 const defaultState = {
     focused:false
 };
 
 export default (state = defaultState,action) => {
-    if(action.type === 'search_focus'){
+    // if(action.type === 'search_focus'){
+    //     return {
+    //         focused:true
+    //     }
+    // }
+    // if(action.type === 'search_blur'){
+    //     return {
+    //         focused:false
+    //     }
+    // }
+     if(action.type === constants.SEARCH_FOCUS){
         return {
             focused:true
         }
     }
-    if(action.type === 'search_blur'){
+    if(action.type === constants.SEARCH_BLUR){
         return {
             focused:false
         }
