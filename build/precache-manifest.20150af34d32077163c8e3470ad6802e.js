52f7b748b9e34dfca0b4cb6837901780d2ca7b53
losthj <thislost@gmail.com>
2019-10-22 18:55:44 +0800

    update

---
+++ build/precache-manifest.20150af34d32077163c8e3470ad6802e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d45a2dfa28704586791548fd808c7a5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64c1d022f4c8308d6b12",
+    "url": "/static/js/2.f20c5bc4.chunk.js"
+  },
+  {
+    "revision": "446f898a86985bf50839",
+    "url": "/static/js/3.07d8f36c.chunk.js"
+  },
+  {
+    "revision": "b875063d26d9e26bbae0",
+    "url": "/static/js/main.fd8a40fd.chunk.js"
+  },
+  {
+    "revision": "6700631bdb0d23da0c27",
+    "url": "/static/js/runtime~main.faac1ae5.js"
+  }
+]);
